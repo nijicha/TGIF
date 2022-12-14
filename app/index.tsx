ffadf553f6b36ce94e8b9a45626059ee9327c56c
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2022-12-14 18:22:15 +0700

    Add fonts using @fontsource

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './index.css'
 
+import '@fontsource/roboto'
+import '@fontsource/noto-sans-thai'
+import '@fontsource/noto-sans-jp'
+
+// FIXME: Import problems
+import '@fontsource/noto-color-emoji/emoji.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -362,20 +362,14 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
         'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
+        '"Noto Sans Thai"',
+        '"Noto Sans JP"',
         '"Noto Color Emoji"',
+        '"Apple Color Emoji"',
+        '-apple-system',
+        'sans-serif',
+        'system-ui'
       ],
       serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: [
