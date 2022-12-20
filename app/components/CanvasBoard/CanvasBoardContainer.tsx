d447119d5377b340a42b8ea1d7adc1e9a57d158c
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2022-12-20 23:28:46 +0700

    Add actions menu

--- app/components/CanvasBoard/CanvasBoardContainer.tsx
+++ app/components/CanvasBoard/CanvasBoardContainer.tsx
@@ -6,13 +6,16 @@ import { useEventListener, useUpdateEffect, useWindowSize } from 'usehooks-ts'
 
 import { Transition } from '@headlessui/react'
 import {
-  ArrowUpLeftIcon as ArrowUpLeftSolid,
-  DocumentArrowDownIcon as DocumentArrowDownIconSolid,
   MagnifyingGlassPlusIcon as MagnifyingGlassPlusIconSolid,
   MagnifyingGlassMinusIcon as MagnifyingGlassMinusIconSolid,
 } from '@heroicons/react/24/solid'
 
-import { Image, Text, TickCircle } from 'iconsax-react'
+import {
+  ArrowUpLeftIcon as ArrowUpLeftOutline,
+  PhotoIcon as PhotoIconOutline,
+} from '@heroicons/react/24/outline'
+
+import { ElementPlus, Text, TickCircle } from 'iconsax-react'
 
 import CandyCane from '../../assets/images/sprites/xmas/candy-cane.png'
 import LightWire from '../../assets/images/sprites/xmas/lights.png'
@@ -22,7 +25,7 @@ import SantaClaus from '../../assets/images/sprites/xmas/santa-claus.png'
 
 import Human from '../../assets/images/sprites/human/human.svg'
 
-type TMenuState = 'select' | 'addImage' | 'addCircle' | 'addSquare'
+type TActionState = 'select' | 'addImage' | 'addText' | 'addCircle' | 'addSquare'
 type TSaveState = 'editing' | 'saving' | 'saved'
 
 const CanvasBoardContainer = () => {
@@ -30,7 +33,7 @@ const CanvasBoardContainer = () => {
 
   const [isLoading, setIsLoading] = useState(false)
   const [saveState, setSaveState] = useState<TSaveState>('editing')
-  const [menuState, _setMenuState] = useState<TMenuState>('select')
+  const [actionState, setActionState] = useState<TActionState>('select')
 
   const { editor, onReady } = useFabricJSEditor()
   const { height } = useWindowSize()
@@ -67,51 +70,32 @@ const CanvasBoardContainer = () => {
     setIsLoading(false)
   }, [editor?.canvas, height])
 
-  const addRect = () => {
-    const rect = new fabric.Rect({
-      height: 280,
-      width: 200,
-      fill: 'white',
-    })
-    editor?.canvas.add(rect)
-    editor?.canvas.renderAll()
-  }
-
-  const addImage = () => {
+  const addImage = (x: number, y: number) => {
     const randomImage = [CandyCane, LightWire, ChristmasTree, Snowman, SantaClaus, Human]
 
     fabric.Image.fromURL(randomImage[Math.floor(Math.random() * 6)], (image) => {
       image.scale(0.75)
+      image.top = y
+      image.left = x
       editor?.canvas.add(image)
       editor?.canvas.renderAll()
     })
     return
   }
 
-  const onKeyDownEvent = (event: KeyboardEvent) => {
-    event.stopImmediatePropagation()
-
-    keysPressed[event.key] = true
-
-    if (
-      keysPressed['z'] &&
-      (keysPressed['Ctrl'] || keysPressed['Meta']) &&
-      keysPressed['Shift']
-    ) {
-      console.log('redo')
-      console.log(keysPressed)
-    } else if (keysPressed['z'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
-      console.log('undo')
-      console.log(keysPressed)
-    } else if (keysPressed['s'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
-      saveCanvasToLocalStorage()
-    }
-  }
-
-  const onOnKeyUpEvent = (event: KeyboardEvent) => {
-    event.stopImmediatePropagation()
-
-    delete keysPressed[event.key]
+  const addSquare = (x: number, y: number) => {
+    const rect = new fabric.Rect({
+      top: y,
+      left: x,
+      height: 200,
+      width: 200,
+      fill: 'white',
+      borderColor: '#000000',
+      borderScaleFactor: 1,
+      hasBorders: true,
+    })
+    editor?.canvas.add(rect)
+    editor?.canvas.renderAll()
   }
 
   useEffect(() => {
@@ -147,9 +131,62 @@ const CanvasBoardContainer = () => {
     updateCanvasSize()
   }, [updateCanvasSize])
 
-  // Document event Listener
-  useEventListener('keydown', onKeyDownEvent)
-  useEventListener('keyup', onOnKeyUpEvent)
+  useEventListener('keydown', (event: KeyboardEvent) => {
+    event.stopImmediatePropagation()
+
+    keysPressed[event.key] = true
+
+    if (
+      keysPressed['z'] &&
+      (keysPressed['Ctrl'] || keysPressed['Meta']) &&
+      keysPressed['Shift']
+    ) {
+      console.log('redo')
+    } else if (keysPressed['z'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
+      console.log('undo')
+    } else if (keysPressed['s'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
+      saveCanvasToLocalStorage()
+    } else if (keysPressed['Backspace'] || keysPressed['Delete']) {
+      console.log('delete object')
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      editor?.canvas.remove(editor?.canvas.getActiveObject())
+      editor?.canvas.renderAll()
+    }
+  })
+
+  useEventListener('keyup', (event: KeyboardEvent) => {
+    event.stopImmediatePropagation()
+
+    delete keysPressed[event.key]
+  })
+
+  useEventListener('mousedown', (event: MouseEvent) => {
+    if (actionState !== 'select' && isNoActiveObject()) {
+      switch (actionState) {
+        case 'addImage':
+          addImage(event.clientX, event.clientY)
+          break
+        case 'addText':
+          break
+        case 'addCircle':
+          break
+        case 'addSquare':
+          addSquare(event.clientX, event.clientY)
+          break
+      }
+
+      setActionState('select')
+    }
+  })
+
+  function isActiveObject() {
+    return !!editor?.canvas.getActiveObject()
+  }
+
+  function isNoActiveObject() {
+    return !isActiveObject()
+  }
 
   if (isLoading) {
     return <div>loading...</div>
@@ -162,113 +199,130 @@ const CanvasBoardContainer = () => {
           className="flex rounded-lg border border-amber-500"
           onReady={onReady}
         />
-        <div className="absolute flex flex-row justify-between rounded-lg bg-transparent">
-          <div className="btn-group m-2.5">
-            <div
-              className={menuState === 'select' ? 'btn-active btn' : 'btn'}
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                addRect()
-              }}
-              onKeyDown={(event) => {
-                // TODO: Add check from kbd focus event is this button
-                if (event.key === 'Enter') {
-                  addRect()
+        <div className="absolute w-full">
+          <div className="flex flex-row justify-between rounded-lg bg-transparent p-4">
+            <div className="btn-group">
+              <div
+                className={
+                  actionState === 'select' ? 'btn-active btn-sm btn' : 'btn-sm btn'
                 }
-              }}
-            >
-              <ArrowUpLeftSolid className="h-6 w-6" aria-hidden="true" />
-            </div>
-            <div
-              className="btn"
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                addImage()
-              }}
-              onKeyDown={(event) => {
-                // TODO: Add check from kbd focus event is this button
-                if (event.key === 'Enter') {
-                  addImage()
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                  setActionState('select')
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['Enter']) {
+                    setActionState('select')
+                  }
+                }}
+              >
+                <ArrowUpLeftOutline className="h-4 w-4" aria-hidden="true" />
+              </div>
+              <div
+                className={
+                  actionState === 'addImage' ? 'btn-active btn-sm btn' : 'btn-sm btn'
                 }
-              }}
-            >
-              <Image
-                className="border-neutral-900 rounded border p-1"
-                size={24}
-                color={'#ffffff'}
-                variant="Broken"
-              />
-            </div>
-            <div
-              className="btn"
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                addImage()
-              }}
-              onKeyDown={(event) => {
-                // TODO: Add check from kbd focus event is this button
-                if (event.key === 'Enter') {
-                  addImage()
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                  setActionState('addImage')
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['Enter']) {
+                    setActionState('addImage')
+                  }
+                }}
+              >
+                <PhotoIconOutline className="h-4 w-4" aria-hidden="true" />
+              </div>
+              <div
+                className={
+                  actionState === 'addText' ? 'btn-active btn-sm btn' : 'btn-sm btn'
                 }
-              }}
-            >
-              <Text size={24} color={'#ffffff'} variant="Broken" />
-            </div>
-            <div
-              className="btn"
-              role="button"
-              tabIndex={0}
-              onClick={() => {
-                saveCanvasToLocalStorage()
-              }}
-              onKeyDown={(event) => {
-                // TODO: Add check from kbd focus event is this button
-                if (event.key === 'Enter') {
-                  saveCanvasToLocalStorage()
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                  setActionState('addText')
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['Enter']) {
+                    setActionState('addText')
+                  }
+                }}
+              >
+                <Text size={16} color={'#ffffff'} variant="Broken" />
+              </div>
+              <div
+                className={
+                  actionState === 'addCircle' ? 'btn-active btn-sm btn' : 'btn-sm btn'
                 }
-              }}
-            >
-              <DocumentArrowDownIconSolid className="h-4 w-4" aria-hidden="true" />
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                  setActionState('addCircle')
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['Enter']) {
+                    setActionState('addCircle')
+                  }
+                }}
+              >
+                <ElementPlus size={16} color={'#ffffff'} variant="Broken" />
+              </div>
+              <div
+                className={
+                  actionState === 'addSquare' ? 'btn-active btn-sm btn' : 'btn-sm btn'
+                }
+                role="button"
+                tabIndex={0}
+                onClick={() => {
+                  setActionState('addSquare')
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['Enter']) {
+                    setActionState('addSquare')
+                  }
+                }}
+              >
+                <ElementPlus size={16} color={'#ffffff'} variant="Broken" />
+              </div>
             </div>
-          </div>
-          <div className="input-group-sm input-group m-2.5">
-            <div
-              className="btn"
-              role="button"
-              tabIndex={0}
-              onClick={(event) => {
-                event.preventDefault()
+            <div className="btn-group">
+              <div
+                className="btn-sm btn"
+                role="button"
+                tabIndex={0}
+                onClick={(event) => {
+                  event.preventDefault()
 
-                editor?.zoomOut()
-              }}
-              onKeyDown={(event) => {
-                if (keysPressed['-'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
                   editor?.zoomOut()
-                }
-              }}
-            >
-              <MagnifyingGlassMinusIconSolid className="h-4 w-4" aria-hidden="true" />
-            </div>
-            <input type="number" placeholder="100%" className="input" />
-            <div
-              className="btn"
-              role="button"
-              tabIndex={0}
-              onClick={(event) => {
-                event.preventDefault()
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['-'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
+                    editor?.zoomOut()
+                  }
+                }}
+              >
+                <MagnifyingGlassMinusIconSolid className="h-4 w-4" aria-hidden="true" />
+              </div>
+              <div
+                className="btn-sm btn"
+                role="button"
+                tabIndex={0}
+                onClick={(event) => {
+                  event.preventDefault()
 
-                editor?.zoomIn()
-              }}
-              onKeyDown={(event) => {
-                if (keysPressed['+'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
                   editor?.zoomIn()
-                }
-              }}
-            >
-              <MagnifyingGlassPlusIconSolid className="h-4 w-4" aria-hidden="true" />
+                }}
+                onKeyDown={() => {
+                  if (keysPressed['+'] && (keysPressed['Ctrl'] || keysPressed['Meta'])) {
+                    editor?.zoomIn()
+                  }
+                }}
+              >
+                <MagnifyingGlassPlusIconSolid className="h-4 w-4" aria-hidden="true" />
+              </div>
             </div>
           </div>
         </div>
@@ -285,7 +339,7 @@ const CanvasBoardContainer = () => {
         <div className="toast-center toast toast-top">
           <div className="alert alert-success">
             <div>
-              <TickCircle size={24} fill={'#ffffff'} variant="Broken" />
+              <TickCircle size={16} fill={'#ffffff'} variant="Broken" />
               <span>Saved!</span>
             </div>
           </div>
